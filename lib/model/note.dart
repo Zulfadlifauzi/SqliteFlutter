04527a4de682ef302bab2f3297cf9d37521b0bd6
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-10-29 19:35:38 +0800

    add list notes,create,update,delete and readall

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,6 +1,14 @@
 const String tableNotes = 'notes';
 
 class NoteFields {
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    time
+  ];
   static const String id = '_id';
   static const String isImportant = 'isImportant';
   static const String number = 'number';
@@ -35,13 +43,23 @@ class Note {
     DateTime? reatedTime,
   }) =>
       Note(
-          id: id ?? this.id,
-          isImportant: isImportant ?? this.isImportant,
-          number: number ?? this.number,
-          title: title ?? this.title,
-          description: description ?? this.description,
-          createdTime: createdTime ?? createdTime,
-          );
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? createdTime,
+      );
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        isImportant: json[NoteFields.isImportant]==1,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.time]as String
+        )
+      );
 
   Map<String, Object?> toJson() => {
         NoteFields.id: id,
---
+++ lib/screens/home_screens.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:sqliteflutter/db/notes_database.dart';
+import 'package:sqliteflutter/model/note.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+    super.dispose();
+  }
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+    notes = await NotesDatabase.instance.readAllNotes();
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
